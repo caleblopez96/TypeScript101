06f55f93480fc8998d238846fc6bc1e3d482bb6e
caleblopez96 <caleblopez96@gmail.com>
2025-02-05 21:04:26 -0700

    working on finishing type annotations

---
+++ Type Annotation/Object Types/Object/object.ts
@@ -0,0 +1,8 @@
+// EXAMPLE:
+let person: {name: string, age: number} = {
+  name: "caleb",
+  age: 28
+}
+
+// EXAMPLE: Objects with Optional Properties
+let user: { name: string, email?: string} = {name: "caleb"}
---
+++ Type Annotation/Object Types/Tuple/tuple.ts
@@ -0,0 +1,4 @@
+// fixed length arrays
+
+// EXAMPLE:
+let nameAndAge: [string, number] = ["Caleb", 28]
