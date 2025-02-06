752ae3cb6448133d30ba47b0485c8b618274d1ff
caleblopez96 <caleblopez96@gmail.com>
2025-02-05 19:36:37 -0700

    added new files

---
+++ Data Types/Primitive Types/Boolean/boolean.ts
@@ -0,0 +1,12 @@
+// Boolean variables represent true or false values
+
+// use the type annotation boolean to use the type of boolean
+
+// EXAMPLE:
+let isNightMode: boolean = false
+
+//! CANNOT reassign to different type
+// isNightMode = "true" --> Type 'string' is not assignable to type 'boolean'.
+
+//* Can reassign to value of same type
+isNightMode = true
---
+++ Type Inference/typeInference.ts
@@ -0,0 +1,7 @@
+// type inference refers to Typescripts compilers ability to infer types from certain values in your code.
+
+// typescript can remember a values type even if you didn't provide a type annotation, and it will enforce that type moving forward
+
+// EXAMPLE:
+let x = 35
+// x = "thirty six" --> Type 'string' is not assignable to type 'number'.
